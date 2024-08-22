5e5cb7b84c2456943fd5a0f1c21bb5247a7ffb0e
gerry-mandering <kimmen1119@gmail.com>
2024-08-22 18:20:49 +0900

    Feat: green deployment test

--- src/main/java/com/example/ecsdeploy/IndexController.java
+++ src/main/java/com/example/ecsdeploy/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.ecsdeploy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
+    @Value("${test-value}")
+    private String testValue;
+
     @GetMapping("/")
     @ResponseBody
     public String index() {
-        return "This is Second Deployment!";
+        return "This is Green Deployment! " + testValue;
     }
 }
